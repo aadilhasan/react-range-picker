7dc4692cb1893ddf4fe7ccc8bea61794bf944145
aadil hasan <aadil.hasan07@gmail.com>
2018-09-17 17:56:51 +0530

    added: working production webpack config

---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+
+// Do this as the first thing so that any code reading it knows the right env.
+process.env.BABEL_ENV = 'production';
+process.env.NODE_ENV = 'production';
+module.exports = {
+  entry: './src/components/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    modules: [path.resolve('./src'), path.resolve('./node_modules')],
+    alias: {
+      // my alias
+      utils: path.resolve(__dirname, './src/utils/index.js')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js|jsx$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|bower_components|build)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.scss$/,
+        include: path.resolve(__dirname, 'src'),
+        use: [
+          {
+            loader: 'style-loader' // creates style nodes from JS strings
+          },
+          {
+            loader: 'css-loader' // translates CSS into CommonJS
+          },
+          {
+            loader: 'sass-loader' // compiles Sass to CSS
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              // Necessary for external CSS imports to work
+              // https://github.com/facebookincubator/create-react-app/issues/2677
+              ident: 'postcss',
+              plugins: () => [
+                require('postcss-flexbugs-fixes'),
+                autoprefixer({
+                  browsers: [
+                    '>1%',
+                    'last 4 versions',
+                    'Firefox ESR',
+                    'not ie < 9' // React doesn't support IE8 anyway
+                  ],
+                  flexbox: 'no-2009'
+                })
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  },
+  externals: {
+    react: 'commonjs react',
+    'react-dom': 'commonjs react-dom'
+  }
+};
