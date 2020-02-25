c1d24ad8b8cc675f0021238ceefef6dd1b9656e4
Aadil Hasan <aadil.hasan07@gmail.com>
2020-02-26 03:04:29 +0530

    update definiations

--- src/components/index.d.ts
+++ src/components/index.d.ts
@@ -4,12 +4,19 @@ type Dates = {
   endDate: Date;
 };
 
+type DefaultValue = {
+  startDate: Date;
+  endDate?: Date;
+};
+
 type FooterParams = Dates & { close: Function; today: Function };
 
 export interface Props {
+  defaultValue?: DefaultValue;
   selectTime?: boolean;
   onDateSelected?: Function;
   onClose?: Function;
+  closeOnSelect?: boolean;
   disableRange?: boolean;
   rangeTillEndOfDay?: boolean;
   placeholder?: (Dates) => React.Component;
